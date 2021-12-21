224ff438feaa23b2e94e4e2d762d5c2bc1ff6b7a
NGUYEN THI THANH HANG <1721050227@student.humg.edu.vn>
2021-12-21 12:59:22 +0700

    Create_Datacontext_NTTH227

---
+++ LTQL_1721050227/LTQL_1721050227/Models/LTQLDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050227.Models
+{
+    public class LTQLDb : DbContext
+    {
+        public LTQLDb() : base("LTQLDb")
+        {
+        }
+    }
+}
