2eb4f018f8fab8b6ae2e269b764a87d01ed4da84
NGUYEN THI THANH HANG <1721050227@student.humg.edu.vn>
2021-12-21 13:47:28 +0700

    Create_Login_NTTH227

---
+++ LTQL_1721050227/LTQL_1721050227/Controllers/AccountsController.cs
@@ -0,0 +1,55 @@
+﻿using LTQL_1721050227.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LTQL_1721050227.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Account acc, string returnUrl)
+        {
+            if (acc.username == "admin" && acc.password == "123456")
+            {
+                FormsAuthentication.SetAuthCookie(acc.username, true);
+                return RedirectToLocal(returnUrl);
+            }
+            return RedirectToAction("Index", "Sanphams");
+        }
+
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+    }
+}
